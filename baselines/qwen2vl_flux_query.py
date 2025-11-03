5bf34db4df504109861cdb87c712c28fdb912e6e
ChandradithyaJ <chandradithya@gmail.com>
2025-11-03 17:35:48 -0500

    code to query qwen2vl-flux

--- baselines/qwen2vl_flux_query.py
+++ baselines/qwen2vl_flux_query.py
@@ -9,3 +9,28 @@ hf_access_token = os.getenv("HF_ACCESS_TOKEN")
 
 # Download model checkpoints from Hugging Face
 snapshot_download("Djrango/Qwen2vl-Flux", token=hf_access_token)
+
+# instantiate the model
+model = FluxModel(device="cuda")
+
+def generate_image(input_image, prompt):
+    # Text-Guided Blending mode of Qwen2VL-Flux
+    outputs = model.generate(
+        input_image_a=input_image,
+        prompt=prompt,
+        mode="variation",
+        guidance_scale=7.5
+    )
+    return outputs
+
+if __name__ == "__main__":
+    use_test = True # use the test folder examples
+
+    if use_test:
+        input_image = open('../test/test_image_input.jpg', 'rb')
+        with open('../test/prompt.txt', 'rb') as f:
+            prompt = f.read()
+
+        output_image = generate_image(input_image, prompt)
+
+        print(output_image)
