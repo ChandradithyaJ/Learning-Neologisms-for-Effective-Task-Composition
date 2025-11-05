23b07a98be87f6f710b4fc15fdd693914c343dbf
ChandradithyaJ <chandradithya@gmail.com>
2025-11-05 18:06:22 -0500

    updated requirements and coded pipeline for https://huggingface.co/black-forest-labs/FLUX.1-Kontext-dev

--- baselines/flux1kontext.py
+++ baselines/flux1kontext.py
@@ -1,31 +1,17 @@
-from huggingface_hub import snapshot_download
-from qwen2vl_flux.model import FluxModel
-import os
-from dotenv import load_dotenv
 import torch
+from diffusers import FluxKontextPipeline
+from diffusers.utils import load_image
 
-# get the HuggingFace access token
-load_dotenv()
-hf_access_token = os.getenv("HF_ACCESS_TOKEN")
-
-# Download model checkpoints from Hugging Face
-snapshot_download("Djrango/Qwen2vl-Flux",
-    token=hf_access_token,
-    local_dir="./checkpoints/qwen2vl_flux"
-)
-
-# instantiate the model
-model = FluxModel(device="cuda")
+pipe = FluxKontextPipeline.from_pretrained("black-forest-labs/FLUX.1-Kontext-dev", torch_dtype=torch.bfloat16)
+pipe.to("cuda")
 
 def generate_image(input_image, prompt):
-    # Text-Guided Blending mode of Qwen2VL-Flux
-    outputs = model.generate(
-        input_image_a=input_image,
+    output_image = pipe(
+        image=input_image,
         prompt=prompt,
-        mode="variation",
-        guidance_scale=7.5
-    )
-    return outputs
+        guidance_scale=2.5
+    ).images[0]
+    return output_image
 
 def save_images(prefix, images, output_dir):
     os.makedirs(output_dir, exist_ok=True)
