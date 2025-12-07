cef49623820d841b56730eb472fd66d5ff201939
ChandradithyaJ <chandradithya@gmail.com>
2025-12-07 17:21:54 -0500

    fixed unequal image resolution issue

--- metrics.py
+++ metrics.py
@@ -7,6 +7,9 @@ import numpy as np
 import open_clip
 import os
 
+FIXED_RESOLUTION = (256, 256)
+resize_transform = transforms.Resize(FIXED_RESOLUTION, interpolation=transforms.InterpolationMode.BICUBIC)
+
 def compute_ssim(generated_images, ground_truth_images):
     """
     Requires ground truth images
@@ -17,8 +20,8 @@ def compute_ssim(generated_images, ground_truth_images):
     ssim = SSIM(data_range=1.0)
 
     for gen_img, gt_img in zip(generated_images, ground_truth_images):
-        gen_img = Image.open(gen_img)
-        gt_img = Image.open(gt_img)
+        gen_img = resize_transform(Image.open(gen_img).convert("RGB"))
+        gt_img  = resize_transform(Image.open(gt_img).convert("RGB"))
 
         gen_img = to_tensor(gen_img).unsqueeze(0)
         gt_img = to_tensor(gt_img).unsqueeze(0)
@@ -38,8 +41,8 @@ def compute_psnr(generated_images, ground_truth_images):
     psnr = PSNR(data_range=1.0)
 
     for gen_img, gt_img in zip(generated_images, ground_truth_images):
-        gen_img = Image.open(gen_img)
-        gt_img = Image.open(gt_img)
+        gen_img = resize_transform(Image.open(gen_img).convert("RGB"))
+        gt_img  = resize_transform(Image.open(gt_img).convert("RGB"))
 
         gen_img = to_tensor(gen_img).unsqueeze(0)
         gt_img = to_tensor(gt_img).unsqueeze(0)
@@ -137,30 +140,30 @@ def CLIP_direction_similarity(generated_images, original_images, edit_prompts):
     model = model.to(device).eval()
 
     gen_tensors = []
-    gt_tensors = []
+    orig_tensors = []
     prompts = []
 
-    for gen_path, gt_path, prompt_path in zip(generated_images, ground_truth_images, edit_prompts):
+    for gen_path, orig_path, prompt_path in zip(generated_images, original_images, edit_prompts):
         gen_img = preprocess(Image.open(gen_path).convert("RGB"))
-        gt_img  = preprocess(Image.open(gt_path).convert("RGB"))
+        orig_img  = preprocess(Image.open(orig_path).convert("RGB"))
         prompt = open(prompt_path, 'r').read()
 
         gen_tensors.append(gen_img)
-        gt_tensors.append(gt_img)
+        orig_tensors.append(orig_img)
         prompts.append(prompt)
 
     gen_batch = torch.stack(gen_tensors).to(device)
-    gt_batch  = torch.stack(gt_tensors).to(device)
+    orig_batch  = torch.stack(orig_tensors).to(device)
 
     with torch.no_grad():
         gen_features = model.encode_image(gen_batch)
-        gt_features  = model.encode_image(gt_batch)
+        orig_features  = model.encode_image(orig_batch)
 
         gen_features = F.normalize(gen_features, dim=-1)
-        gt_features  = F.normalize(gt_features, dim=-1)
+        orig_features  = F.normalize(orig_features, dim=-1)
 
     # compute delta image direction
-    delta_img = gen_features - gt_features
+    delta_img = gen_features - orig_features
     delta_img = F.normalize(delta_img, dim=-1)
 
     # encode prompts
@@ -177,12 +180,18 @@ def CLIP_direction_similarity(generated_images, original_images, edit_prompts):
 if __name__ == "__main__":
     generated_images_folder = "../scratch/DL_data/images/instruct_pix2pix_outputs_neologism_and_1stepsPerImage_80trainImages_100epochs_8denoisingSteps_ckpt70"
     ground_truth_images_folder = "../scratch/DL_data/images/final"
-    original_images = "../scratch/DL_data/images/original"
+    original_images_folder = "../scratch/DL_data/images/original"
     edit_prompts_folder = "../scratch/DL_data/prompts/composite"
 
-    generated_images = os.listdir(generated_images_folder)
-    ground_truth_images = os.listdir(ground_truth_images_folder)
-    edit_prompts = os.listdir(edit_prompts_folder)
+    generated_images = sorted(os.listdir(generated_images_folder))
+    ground_truth_images = sorted(os.listdir(ground_truth_images_folder))
+    original_images = sorted(os.listdir(original_images_folder))
+    edit_prompts = sorted(os.listdir(edit_prompts_folder))
+
+    generated_images = [os.path.join(generated_images_folder, f) for f in generated_images]
+    ground_truth_images = [os.path.join(ground_truth_images_folder, f) for f in ground_truth_images]
+    original_images = [os.path.join(ground_truth_images_folder, f) for f in original_images]
+    edit_prompts = [os.path.join(edit_prompts_folder, f) for f in edit_prompts]
 
     ssim = compute_ssim(generated_images, ground_truth_images)
     psnr = compute_psnr(generated_images, ground_truth_images)
